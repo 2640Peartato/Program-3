0879dce70090a4938b536d5a2407dceaa2c586de
Ayanna Sanges-Chu <89478813+aksangeschu@users.noreply.github.com>
2023-05-07 12:22:52 -0700

    Made student name corrections
    made it look a bit cleaner

--- letterGradeMem.asm
+++ letterGradeMem.asm
@@ -1,19 +1,20 @@
 #Names: Catalina Davis, Kenneth Hung, Ayanna Sanges-Chu
 #Class 2640.02
 #Date: May 7, 2023
-#Objectives: given array int scores = [32, 56, 78, 66, 88, 90, 93, 100, 101, 82]
+#Objectives for Task 1:
+# - given array int scores = [32, 56, 78, 66, 88, 90, 93, 100, 101, 82]
 # - return a grade for each element
 # - a main label, a looping label, and an exit label
 # - a loop counter
 # - a separate case for grades above 100 (i.e. prints out "A with Extra Credit")
 # - your name printed out after all the grades are read 
-# - (and before the program exits)
+# 	before the program exits
 
 .macro exitMess
 #print out exit message
 li $v0, 4
 .data
-student: .asciiz "McLOVIN"
+student: .asciiz "Catalina Davis\nKenneth Hung\nAyanna Sanges-Chu"
 exiting: .asciiz "\nThe program will now exit."
 newLine: .asciiz "\n"
 .text
@@ -46,15 +47,11 @@ c: .asciiz "C\n"
 d: .asciiz "D\n"
 f: .asciiz "F\n"
 extraCredit: .asciiz "A with Extra Credit!\n"
-
-student: .asciiz "McLOVIN"
-
 .text
 main:
 	#initialize loop counter and array index
 	li $t0, 0	#loop index
 	li $t1, 0	#array index
-
 loop:
 	#check if loop counter is equal to
 	#the number of elements in the array
@@ -86,26 +83,20 @@ loop:
 	bgt $t3, 59, D
 	
 	#when grade is f
-	
 	li $v0, 4
 	la $a0, f
 	syscall
 	
 	j Increment
-	
 A: 
 	#display letter grade 'A'
-	
 	li $v0, 4
 	la $a0, a
 	syscall
 	
 	j Increment
-	
 B:
-	
 	#display letter grade 'B'
-	
 	li $v0, 4
 	la $a0, b
 	syscall
@@ -113,31 +104,25 @@ B:
 	j Increment
 C:
 	#display letter grade 'C'
-	
 	li $v0, 4
 	la $a0, c
 	syscall
 
 	j Increment
 D:
-
 	#display letter grade 'D'
-	
 	li $v0, 4
 	la $a0, d
 	syscall
 	
 	j Increment
-	
 EC:
 	#extra credit case
-	
 	li $v0, 4
 	la $a0, extraCredit
 	syscall
 	
 	j Increment
-
 Increment:
 
 	#increment loop counter and array index
@@ -147,11 +132,9 @@ Increment:
 	j loop
 
 exitMessage:
-
+	#call macro
 	exitMess
-	
 	j exit
-	
 exit:
 	#exit the program
 	li $v0, 10
